15b6cd01225e5cf99a209e9e1b3cd8be15b96621
Christina <czhou2@schools.nyc.gov>
2019-09-24 11:47:00 -0400

    dynamic motion with two circles

--- geometry automata.py
+++ geometry automata.py
@@ -1,6 +1,5 @@
 import tkinter as tk
-from scipy.spatial.distance import cdist
-
+import time
 
 root = tk.Tk()
 canvas = tk.Canvas(root, width=300, height=200, bg='black')
@@ -17,29 +16,56 @@ motion='FULLMOON'
 
 coords = (100, 50, 200, 150)
 
-arc_one = canvas.create_arc(coords, start=0, extent=90, outline="white", style="arc")
+# use box width to later move around
+box_width = coords[2] - coords[0]
 
-arc_two = canvas.create_arc(coords, start=90, extent=90, outline="white", style="arc")
+# use arc width for width of 1 component
+# 4 components in 1 box
+arc_width = box_width/2
 
-arc_three = canvas.create_arc(coords, start=180, extent=90, outline="white", style="arc")
+arc_one = canvas.create_arc(coords, start=0, extent=90, outline="white", style="arc",tag=1)
 
-arc_four = canvas.create_arc(coords, start=270, extent=90, outline="white", style="arc")
+arc_two = canvas.create_arc(coords, start=90, extent=90, outline="white", style="arc",tag=2)
+
+arc_three = canvas.create_arc(coords, start=180, extent=90, outline="white", style="arc",tag=3)
+
+arc_four = canvas.create_arc(coords, start=270, extent=90, outline="white", style="arc",tag=4)
+
+'''
+I want to just bundle arcs one to four into a single method, and call it with new coordinates to create many new 
+circles. 
+
+However, I won't be able to call each arc later if I do so.
+'''
 
 # second bounding box to the right
 coords_2 = (100 + 100, 50, 200 + 100, 150)
 
-arc_five = canvas.create_arc(coords_2, start=0, extent=90, outline="white", style="arc")
+arc_five = canvas.create_arc(coords_2, start=0, extent=90, outline="white", style="arc",tag=1)
 
-arc_six = canvas.create_arc(coords_2, start=90, extent=90, outline="white", style="arc")
+arc_six = canvas.create_arc(coords_2, start=90, extent=90, outline="white", style="arc",tag=2)
 
-arc_seven = canvas.create_arc(coords_2, start=180, extent=90, outline="white", style="arc")
+arc_seven = canvas.create_arc(coords_2, start=180, extent=90, outline="white", style="arc",tag=3)
 
-arc_eight = canvas.create_arc(coords_2, start=270, extent=90, outline="white", style="arc")
+arc_eight = canvas.create_arc(coords_2, start=270, extent=90, outline="white", style="arc",tag=4)
 
 
 def bold(event):
     id = event.widget.find_closest(event.x,event.y)[0]
     canvas.itemconfigure(id,width=2.5)
+    canvas.update()
+    time.sleep(.4)
+
+    if direction == 'RIGHT' and motion == 'FULLMOON':
+#         while there are no more new widgets
+        while (id != event.widget.find_closest(event.x + arc_width, event.y)[0]):
+    #         move cursor to the right
+            event.x += arc_width
+            id = event.widget.find_closest(event.x, event.y)[0]
+            canvas.itemconfigure(id, width=2.5)
+            canvas.update()
+            time.sleep(.4)
+
 
 canvas.tag_bind(arc_one,"<Button-1>", bold)
 canvas.tag_bind(arc_two,"<Button-1>", bold)
@@ -53,48 +79,4 @@ canvas.tag_bind(arc_eight,"<Button-1>", bold)
 
 
 root.mainloop()
-
-def arc_one(coords):
-    return canvas.create_arc(coords, start=0, extent=90, outline="white", style="arc")
-
-def arc_two(coords):
-    return canvas.create_arc(coords, start=90, extent=90, outline="white", style="arc")
-
-def arc_three(coords):
-    return canvas.create_arc(coords, start=180, extent=90, outline="white", style="arc")
-
-def arc_four(coords):
-    return canvas.create_arc(coords, start=270, extent=90, outline="white", style="arc")
-
-class Box():
-    def __init__(self, x,y,x1,y1):
-        self.x = x
-        self.y = y
-        self.x1 = x1
-        self.y1 = y1
-        self.coords = x,y,x1,y1
-        self.one = arc_one(coords)
-        self.two = arc_two(coords)
-        self.three = arc_three(coords)
-        self.four = arc_four(coords)
-
-coords = (100, 50, 200, 150)
-unit_1 = Box(coords)
-
-def closest_node(node, nodes):
-    return nodes[cdist([node], nodes).argmin()]
-
-def draw(x,y):
-    point = closest_node([x,y],[(150,100),(200,100),(150,150),(100,00)])
-
-
-def start(event):
-    global lastx, lasty
-    lastx, lasty = event.x, event.y
-    draw(lastx, lasty)
-
-
-
-root.mainloop()
-# canvas.itemconfig(one,fill='red')
 
