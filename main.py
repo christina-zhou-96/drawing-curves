abbc598c702246b99c710d5360fcf2d1fc641e9a
Paradise Lost <christina.zhou.96@gmail.com>
2019-11-03 09:23:30 -0500

    add method stubs for petal inputs

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import tkinter as tk
 import time
 import numpy
+import math
 
 # set hex colors
-# cream = '#fafaeb'
-cream = 'black'
-# umber = '#21201f'
-umber = 'white'
+cream = '#fafaeb'
+# cream = 'black'
+umber = '#21201f'
+# umber = 'white'
 root = tk.Tk()
 canvas = tk.Canvas(root, width=650, height=450, bg=umber)
 canvas.pack(fill="both", expand=True)
@@ -17,7 +18,6 @@ direction='DOWN'
 # note that tkinter canvas starts with an origin in the corner, moving down therefore is *adding* y value,
 # so some of the math may look inverted
 
-motion='HALFMOON'
 
 # change direction or motion
 def apply_directives(event):
@@ -41,125 +41,203 @@ root.bind("<Key>", apply_directives)
 
 WIDTH = 6
 
+def distance(box_coords_1, box_coords_2, axis='x'):
+    """
+    Calculate the distance on an axis between two boxes
+    :param box_coords_1: tuple of floats representing the first box | (x, y)
+    :param box_coords_2: tuple of floats representing the 2nd box | (x, y)
+    :return: float
+    """
+
+    class TwoAxesError(Exception):
+        """Raised when an axis other than y or x is called"""
+        pass
+
+    try:
+        if axis == 'x':
+            return math.fabs(box_coords_1[0] - box_coords_2[0])
+        elif axis == 'y':
+            return math.fabs(box_coords_1[1] - box_coords_2[1])
+        else:
+            raise TwoAxesError
+    except TwoAxesError:
+        print("Passed an axis other than y or x")
+
+def play(neighborhood):
+    """
+    Play once finished giving all the inputs.
+    :param neighborhood: what we are going to copy for the animation. the 4x4
+    neighborhood depicting the current petal state
+    :return: None
+    """
+    if check_for_falling_petal(neighborhood):
+        falling_petal_animation(neighborhood)
+
+def check_for_falling_petal(neighborhood):
+    """
+    Check if the conditions for the falling petal animation are met
+    :return: bool
+    """
+    ##### You can only move forward with the animation if you have exactly 2
+    # petals in the neighborhood. Any more or less looks bad/unnatural
+    if neighborhood.petal_count != 2:
+        pass
+
+    if _correct_petal_distance(neighborhood.petals):
+        falling_petal_animation(neighborhood)
+
+def _correct_petal_distance(petals):
+    """
+    Determine if two petals are at the right distance from each other for the
+    animation to start. The start rule: For petals to start, they must be in
+    a box exactly 1 unit away on the x axis, and 1 unit away on the x axis.
+
+    Petals must be able to reference back to their own box, and then their
+    own box coordinates.
+    :param petals: tuple | (petal1, petal2)
+    :return: None
+    """
+    if distance(petals[0].box.coords, petals[1].box.coords, axis='x') and \
+            distance(petals[0].box.coords, petals[1].box.coords, axis='y'):
+        return True
+    else:
+        return False
+
+def falling_petal_animation(neighborhood):
+    """
+    Classic animation of petals falling slightly to the left in a breeze.
+    Transpose the neighborhood down and to the left.
+    :return: None
+    """
+    print("At falling petal animation!")
+    pass
+
 
 
 # apply bold to line
-def bold(event):
+def bold(event=None):
     # initial boldness
-    # find arc user meant
-    id = event.widget.find_closest(event.x,event.y)[0]
-    # retrieve arc tag
-    tag = canvas.gettags(id)[1]
-    # bold arc
-    canvas.itemconfigure(id,width=WIDTH)
-    # redraw canvas
-    canvas.update()
-    # give time to make each drawing piecemeal
-    time.sleep(.5)
+    if not event:
+        ##### The user is not passing an event. This is the primary use case
+        # of this application
+            pass
 
-    if motion == 'HALFMOON':
+    else:
+        ##### The user is passing an event!
+        # find arc user meant
+        id = event.widget.find_closest(event.x,event.y)[0]
+        # retrieve arc tag
+        tag = canvas.gettags(id)[1]
+        # bold arc
+        canvas.itemconfigure(id,width=WIDTH)
+        # redraw canvas
+        canvas.update()
+        # give time to make each drawing piecemeal
+        time.sleep(.5)
 
-        # find within the next enclosed box in the right, the arc with a tag that fits the motion type so long as
-        # there are no more arcs to the right
-        set_a = 0
-        set_b = 0
+        if motion == 'HALFMOON':
 
-        # sets inverse depending on vertical or horizontal direction
-        if direction == 'RIGHT' or direction == 'LEFT':
-            set_a = ['1','2']   # tags are in type string, so we match the type
-            set_b = ['3','4']
-        if direction == 'UP' or direction == 'DOWN':
-            set_a = ['1','4']   # tags are in type string, so we match the type
-            set_b = ['2','3']
+            # find within the next enclosed box in the right, the arc with a tag that fits the motion type so long as
+            # there are no more arcs to the right
+            set_a = 0
+            set_b = 0
 
-        current_set = []
+            # sets inverse depending on vertical or horizontal direction
+            if direction == 'RIGHT' or direction == 'LEFT':
+                set_a = ['1','2']   # tags are in type string, so we match the type
+                set_b = ['3','4']
+            if direction == 'UP' or direction == 'DOWN':
+                set_a = ['1','4']   # tags are in type string, so we match the type
+                set_b = ['2','3']
 
-        # check to see what kind of curve this is
-        if tag in set_a:
-            current_set = set_a
-        else:
-            current_set = set_b
+            current_set = []
 
-        # TODO: Sometimes takes an arc that shouldn't be within the bounding box, but can't consistently
-        #  replicate this. find out way and fix
-        # possibly when you double click?
+            # check to see what kind of curve this is
+            if tag in set_a:
+                current_set = set_a
+            else:
+                current_set = set_b
 
-        # direction logic
-        directional_additive = 0
-        if direction == 'RIGHT':
-            directional_additive = numpy.array([arc_width,0])
-        if direction == 'LEFT':
-            directional_additive = numpy.array([-arc_width,0])
-        if direction == 'UP':
-            directional_additive = numpy.array([0,-arc_width])
-        if direction == 'DOWN':
-            directional_additive = numpy.array([0,arc_width])
-        prev_id = 0
-        # TODO: buggy while. figure out previous id while loop?
-        # when there are no more arcs to the desired direction
-        while (id != prev_id):
-            print('in while')
-            print('x is ' + str(event.x))
-            print('x bound is ' + str(canvas.winfo_width() - arc_width))
-            print('y is ' + str(event.y))
-            print('y bound is ' + str(canvas.winfo_height() - arc_width))
-            # set up variables to find next coordinates
-            current_box_coords = numpy.array(canvas.coords(id))
-            # box is too big, we just want the arc box
-            normalizer = 0
-            if tag == '1': # take upper right of box
-                normalizer = numpy.array([arc_width,0,0,-arc_width])
-            if tag == '2': # take upper left of box
-                normalizer = numpy.array([0,0,-arc_width,-arc_width])
-            if tag == '3': # take lower left of box
-                normalizer = numpy.array([0,arc_width,-arc_width,0])
-            if tag == '4': # take lower right of box
-                normalizer = numpy.array([arc_width,arc_width,0,0])
-            current_arc_coords = current_box_coords + normalizer
-            next_coords_additive = 0
-            # directional logic
+            # TODO: Sometimes takes an arc that shouldn't be within the bounding box, but can't consistently
+            #  replicate this. find out way and fix
+            # possibly when you double click?
+
+            # direction logic
+            directional_additive = 0
             if direction == 'RIGHT':
-                next_coords_additive = numpy.array([arc_width,0,arc_width,0])
+                directional_additive = numpy.array([arc_width,0])
             if direction == 'LEFT':
-                next_coords_additive = numpy.array([-arc_width,0,-arc_width,0])
+                directional_additive = numpy.array([-arc_width,0])
             if direction == 'UP':
-                next_coords_additive = numpy.array([0,-arc_width,0,-arc_width])
+                directional_additive = numpy.array([0,-arc_width])
             if direction == 'DOWN':
-                next_coords_additive = numpy.array([0,arc_width,0,arc_width])
-            # tkinter's find_enclosed method will exclude any objects it finds right at the perimeter, so make the perimeter slightly larger
-            boundaries_additive = numpy.array([-1,-1,1,1])
-            # obtain the next coordinates
-            next_coords = current_arc_coords + next_coords_additive + boundaries_additive
+                directional_additive = numpy.array([0,arc_width])
+            prev_id = 0
+            # TODO: buggy while. figure out previous id while loop?
+            # when there are no more arcs to the desired direction
+            while (id != prev_id):
+                print('in while')
+                print('x is ' + str(event.x))
+                print('x bound is ' + str(canvas.winfo_width() - arc_width))
+                print('y is ' + str(event.y))
+                print('y bound is ' + str(canvas.winfo_height() - arc_width))
+                # set up variables to find next coordinates
+                current_box_coords = numpy.array(canvas.coords(id))
+                # box is too big, we just want the arc box
+                normalizer = 0
+                if tag == '1': # take upper right of box
+                    normalizer = numpy.array([arc_width,0,0,-arc_width])
+                if tag == '2': # take upper left of box
+                    normalizer = numpy.array([0,0,-arc_width,-arc_width])
+                if tag == '3': # take lower left of box
+                    normalizer = numpy.array([0,arc_width,-arc_width,0])
+                if tag == '4': # take lower right of box
+                    normalizer = numpy.array([arc_width,arc_width,0,0])
+                current_arc_coords = current_box_coords + normalizer
+                next_coords_additive = 0
+                # directional logic
+                if direction == 'RIGHT':
+                    next_coords_additive = numpy.array([arc_width,0,arc_width,0])
+                if direction == 'LEFT':
+                    next_coords_additive = numpy.array([-arc_width,0,-arc_width,0])
+                if direction == 'UP':
+                    next_coords_additive = numpy.array([0,-arc_width,0,-arc_width])
+                if direction == 'DOWN':
+                    next_coords_additive = numpy.array([0,arc_width,0,arc_width])
+                # tkinter's find_enclosed method will exclude any objects it finds right at the perimeter, so make the perimeter slightly larger
+                boundaries_additive = numpy.array([-1,-1,1,1])
+                # obtain the next coordinates
+                next_coords = current_arc_coords + next_coords_additive + boundaries_additive
 
-            # obtain list of the next IDs
-            next_ids = event.widget.find_enclosed(*next_coords)
+                # obtain list of the next IDs
+                next_ids = event.widget.find_enclosed(*next_coords)
 
-            # obtain list of the next tags
-            next_tags = [canvas.gettags(next_id)[1] for next_id in next_ids]
+                # obtain list of the next tags
+                next_tags = [canvas.gettags(next_id)[1] for next_id in next_ids]
 
-            last_event_x = event.x
-            prev_id = id
+                last_event_x = event.x
+                prev_id = id
 
-            for next_id,next_tag in zip(next_ids,next_tags):
-                if ((id != next_id) & (next_tag in current_set)):
-                    # move cursor to the desired direction
-                    if direction == 'RIGHT':
-                        event.x += arc_width
-                    if direction == 'LEFT':
-                        event.x -= arc_width
-                    if direction == 'UP':
-                        event.y -= arc_width
-                    if direction == 'DOWN':
-                        event.y += arc_width
+                for next_id,next_tag in zip(next_ids,next_tags):
+                    if ((id != next_id) & (next_tag in current_set)):
+                        # move cursor to the desired direction
+                        if direction == 'RIGHT':
+                            event.x += arc_width
+                        if direction == 'LEFT':
+                            event.x -= arc_width
+                        if direction == 'UP':
+                            event.y -= arc_width
+                        if direction == 'DOWN':
+                            event.y += arc_width
 
-                    # bold the new arc
-                    canvas.itemconfigure(next_id, width=WIDTH)
-                    canvas.update()
-                    time.sleep(.5)
-                    # update current arc
-                    id = event.widget.find_closest(event.x, event.y)[0]
-                    # update current tag
-                    tag = canvas.gettags(id)[1]
+                        # bold the new arc
+                        canvas.itemconfigure(next_id, width=WIDTH)
+                        canvas.update()
+                        time.sleep(.5)
+                        # update current arc
+                        id = event.widget.find_closest(event.x, event.y)[0]
+                        # update current tag
+                        tag = canvas.gettags(id)[1]
 
 # each bounding box is 100 x 100
 class Box():
@@ -193,13 +271,13 @@ coords = numpy.array([0, 0, 100, 100])
 
 
 # use box width to calculate grid indice
-box_width = coords[2] - coords[0]
+BOX_WIDTH = coords[2] - coords[0]
 # grid indice to move around
-grid_indice = box_width/2
+grid_indice = BOX_WIDTH/2
 
 # use arc width for width of 1 component
 # 4 components in 1 box
-arc_width = box_width/2
+arc_width = BOX_WIDTH/2
 
 # make desired size of grid (width, height)
 size=[6*4,4*4]
